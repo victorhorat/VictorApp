00ed30e93c7a1b35e444b91f2ebdc527e05fef6d
Victor Hora <victorhorten@hotmail.com>
2025-06-24 22:19:55 -0300

    refactor: initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -22,5 +18,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
