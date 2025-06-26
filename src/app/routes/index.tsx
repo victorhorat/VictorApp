d359f612ee5e3e6f731bc4d650ecf9ef10bfe9af
Victor Hora <victorhorten@hotmail.com>
2025-06-25 22:25:28 -0300

    refactor: folder structure and routes finalized

---
+++ src/app/routes/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"; // 'Routes' importado diretamente
+import { Dashboard } from '../pages';
+
+export const AppRoutes = () => { 
+  return (
+    // BrowserRouter é o roteador principal que usa a API de histórico do HTML5.
+    <BrowserRouter>
+      <Routes> 
+        <Route path="/" element={<h1>Olá, você está na página inicial!</h1>} />
+        <Route path="/caminho-antigo" element={<Navigate to="/novo-caminho" replace />} />
+        <Route path="/novo-caminho" element={<h2>Esta é a nova página após o redirecionamento!</h2>} />
+        <Route path="/dashboards" element={<Dashboard />} />
+        <Route path="*" element={<h2>404 - Página Não Encontrada</h2>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
