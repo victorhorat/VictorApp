a15dc87673efdb9a9fc02ab2bdc03397f57651ea
Victor Hora <victorhorten@hotmail.com>
2025-08-11 16:23:40 -0300

    feat: context api

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,15 +1,22 @@
 import { useRef } from "react";
 import { Link } from "react-router-dom";
+import { useUsuarioLogado } from "../../shared/hooks";
 
 export const Dashboard = () =>{
     const counterRef = useRef({ counter: 0});
+
+    const {nomeDoUsuario, logout} = useUsuarioLogado();
+
     return(
         <div>
             <p>Dashboard</p>
 
+            <p>{nomeDoUsuario}</p>
+
             <p>Contador: {counterRef.current.counter}</p>
 
             <button onClick={() => counterRef.current.counter++}>Somar</button>
+            <button onClick={logout}>Logout</button>
 
             <Link to="/login">Login</Link>
             <Link to="/login">
--- src/app/shared/contexts/UsuarioLogado.tsx
+++ src/app/shared/contexts/UsuarioLogado.tsx
@@ -1,21 +1,33 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useCallback, useEffect, useState } from "react";
 
 interface IUsuarioLogadoContextData {
     nomeDoUsuario:string;
+    logout: () => void;
 }
 
 interface IUsuarioLogadoProviderProps {
     children: ReactNode;
 }
 
-const UsuarioLogadoContext = createContext<IUsuarioLogadoContextData >({} as IUsuarioLogadoContextData);
+export const UsuarioLogadoContext = createContext<IUsuarioLogadoContextData >({} as IUsuarioLogadoContextData);
 
 export const UsuarioLogadoProvider = ({ children }: IUsuarioLogadoProviderProps) => {
 
+    const [nome, setNome] = useState('');
+
+    useEffect(() => {
+        setTimeout(()=>{
+            setNome('Victor');
+        },1000);
+    });
+
+    const handleLogout = useCallback(() => {
+        console.log('Logout executou')
+    }, []);
+
     return (
-        <UsuarioLogadoContext.Provider value={{nomeDoUsuario: 'Victor'}}>
+        <UsuarioLogadoContext.Provider value={{nomeDoUsuario: nome, logout: handleLogout}}>
             {children}
-            
         </UsuarioLogadoContext.Provider>
-    )
+    );
 }
